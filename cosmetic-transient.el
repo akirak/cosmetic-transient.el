660434cf5fde4c6638cf96fd72b422396890a86e
Akira Komamura <akira.komamura@gmail.com>
2024-11-03 13:04:54 +0900

    feat: Support multiple languages

--- cosmetic-transient.el
+++ cosmetic-transient.el
@@ -59,9 +59,26 @@
   :type '(alist :key-type (symbol :tag "Language name")
                 :value-type plist))
 
+(defcustom cosmetic-transient-string-ts-nodes
+  '((rust "string_content")
+    (typescript "string_fragment"))
+  "Alist of tree-sitter node types for strings."
+  :type '(alist :key-type (symbol :tag "Treesit major mode")
+                :value-type (repeat :tag "List of node types" string)))
+
+(defcustom cosmetic-transient-ts-language-detectors
+  '((typescript . cosmetic-transient-ts-js-language))
+  "Alist of functions that returns the local language of the node."
+  :type '(alist :key-type (symbol :tag "Treesit major mode")
+                :value-type (function :tag "Function that takes a node as an argument")))
+
 (defvar-local cosmetic-transient-language-formatter nil)
 
 (defvar-local cosmetic-transient-docco-status nil)
+
+(defvar-local cosmetic-transient-string-node nil)
+
+(defvar cosmetic-transient-alternative-language nil)
 
 ;;;###autoload (autoload 'cosmetic-transient "cosmetic-transient" nil 'interactive)
 (transient-define-prefix cosmetic-transient ()
@@ -82,7 +99,11 @@
     (lambda ()
       (format "%s" (car cosmetic-transient-language-formatter)))
     :if-non-nil cosmetic-transient-language-formatter
-    :setup-children cosmetic-transient--language-formatter-children]]
+    :setup-children cosmetic-transient--language-formatter-children]
+   ["String at point"
+    :if-non-nil cosmetic-transient-string-node
+    ("-l" cosmetic-transient-other-language)
+    ("fo" "Apply formatter for the language" cosmetic-transient-run-other-formatter)]]
   ["Linter"]
   ["Doc comments"
    :if (lambda ()
@@ -91,6 +112,11 @@
   (interactive)
   (setq cosmetic-transient-language-formatter
         (cosmetic-transient--language-formatter-settings))
+  (let ((node-or-region (cosmetic-transient--string-region-at (point))))
+    (setq cosmetic-transient-string-node node-or-region)
+    (setq cosmetic-transient-alternative-language
+          (when node-or-region
+            (cosmetic-transient--region-language node-or-region))))
   (transient-setup 'cosmetic-transient))
 
 ;;;; Eglot integration
@@ -138,6 +164,99 @@
                 (list transient--default-child-level
                       'transient-suffix
                       plist))))))
+
+;;;; Formatter for another language
+
+(defclass cosmetic-transient-language (transient-variable)
+  ((variable :initarg :variable)
+   (prompt :initarg :prompt)))
+
+(cl-defmethod transient-init-value ((obj cosmetic-transient-language))
+  (let ((value (symbol-value (oref obj variable))))
+    (oset obj value value)
+    (set (oref obj variable) value)))
+
+(cl-defmethod transient-infix-read ((obj cosmetic-transient-language))
+  (intern (completing-read "Language: " cosmetic-transient-language-formatters
+                           nil 'require-match
+                           nil nil nil
+                           (symbol-value (oref obj variable)))))
+
+(cl-defmethod transient-infix-set ((obj cosmetic-transient-language) value)
+  (set (oref obj variable) (oset obj value value)))
+
+(cl-defmethod transient-format-value ((obj cosmetic-transient-language))
+  (if-let (value (oref obj value))
+      (concat
+       (propertize "(" 'face 'transient-inactive-value)
+       (propertize (format "%s" value)
+                   'face 'transient-value)
+       (propertize ")" 'face 'transient-inactive-value))
+    ""))
+
+(transient-define-infix cosmetic-transient-other-language ()
+  :class 'cosmetic-transient-language
+  :variable 'cosmetic-transient-alternative-language
+  :prompt "Language: "
+  :description "Language")
+
+(defun cosmetic-transient-run-other-formatter ()
+  (interactive)
+  (if cosmetic-transient-alternative-language
+      (if-let* ((plist (cdr (assq cosmetic-transient-alternative-language
+                                  cosmetic-transient-language-formatters)))
+                (formatter (plist-get plist :region-formatter)))
+          (pcase (cosmetic-transient--bounds cosmetic-transient-string-node)
+            (`(,beg . ,end)
+             (funcall formatter beg end)
+             (cosmetic-transient--apply-offset beg)))
+        (user-error "No formatter is set"))
+    (user-error "No language is set")))
+
+(defun cosmetic-transient--region-language (node-or-region)
+  (if (treesit-node-p node-or-region)
+      (when-let (func (alist-get (treesit-node-language node-or-region)
+                                 cosmetic-transient-ts-language-detectors))
+        (funcall func node-or-region))))
+
+(defun cosmetic-transient--string-region-at (pos)
+  (if-let* ((treesit-node-types (alist-get (cosmetic-transient--mode-language major-mode)
+                                           cosmetic-transient-string-ts-nodes))
+            (ts-node (and (treesit-available-p)
+                          (treesit-language-at pos)
+                          (treesit-node-at pos))))
+      (when (member (treesit-node-type ts-node)
+                    treesit-node-types)
+        ts-node)
+    (when-let* ((start (ppss-comment-or-string-start (syntax-ppss))))
+      (save-excursion
+        (goto-char start)
+        (forward-sexp)
+        ;; It is assumed that string quotations are single characters.
+        ;; Otherwise, this function malfunctions.
+        (cons (1+ start) (1- (point)))))))
+
+(defun cosmetic-transient--bounds (node-or-region)
+  (if (treesit-node-p node-or-region)
+      (cons (treesit-node-start node-or-region)
+            (treesit-node-end node-or-region))
+    node-or-region))
+
+(defun cosmetic-transient--apply-offset (beg)
+  (save-excursion
+    (let* ((bound (cdr (cosmetic-transient--bounds (cosmetic-transient--string-region-at beg))))
+           (offset (car (posn-col-row (posn-at-point beg)))))
+      (goto-char beg)
+      (end-of-line)
+      (replace-regexp-in-region (rx bol) (make-string offset ?\s) nil bound))))
+
+(defun cosmetic-transient-ts-js-language (node)
+  "Detect the language of \"string_fragment\" node."
+  (when-let* ((parent (treesit-node-parent node))
+              (node1 (treesit-node-prev-sibling parent))
+              (tag (when (equal (treesit-node-type node1) "identifier")
+                     (treesit-node-text node1))))
+    (cdr (assoc tag '(("gql" . graphql))))))
 
 (provide 'cosmetic-transient)
 ;;; cosmetic-transient.el ends here
